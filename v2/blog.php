f242ea18427191a6fbdc14c59921271847480538
carlos-martinez-maqueta <cmartinez.meneses1@gmail.com>
2025-07-01 23:53:41 -0500

    blog

---
+++ v2/blog.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blog | DIINTEC</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+</head>
+
+<body>
+    <header class="header_app">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-2 col-6">
+                    <div class="logo_web">
+                        <a href=""><img src="assets/img/logo.svg" class="img-fluid" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="nav_header">
+                        <ul class="ul_nav">
+                            <li><a href="">Inicio</a></li>
+                            <li><a href="">Servicios </a></li>
+                            <li><a href="">Nosotros</a></li>
+                            <li><a href="">Proyectos</a></li>
+                            <li><a href="">Contacto</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 text-end">
+                    <div class="cto_button">
+                        <a href="">Comienza Ya!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main class="main_black">
+        <section class="main_hv">
+            <div class="section_banner text-center">
+                <div>
+                    <h2 id="text1">Destaca en el mundo digital</h2>
+                </div>
+                <div class="p_info">
+                    <p>Creamos experiencias únicas que conectan, venden y posicionan tu marca en el universo digital. Desde tiendas virtuales hasta estrategias SEO avanzadas, somos tu aliado en la evolución tecnológica.</p>
+                </div>
+                <!-- <div class="button_banner"><button>Comienza tu transformación ahora <img src="assets/img/button.svg" class="mx-2 img-fluid" alt=""></button></div> -->
+            </div>
+        </section>
+
+        <section class="adornos_home py-5">
+            <div class="container">
+                <div class="row align-items-center justify-content-md-center">
+                    <div class="col-lg-4">
+                        <div><img src="assets/img/linea_izquierda.png" class="img-fluid" alt=""></div>
+                    </div>
+                    <div class="col-lg-1">
+                        <div class="text-center"><img src="assets/img/icon_medio.png" class="img-fluid" alt=""></div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div><img src="assets/img/linea_derecha.png" class="img-fluid" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="clientes blog py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h3>Blog</h3>
+                    </div>
+                    <div class="col-12 carrusel_clientes py-5">
+                        <div class="row">
+                            <div class="col-3 item_cliente">
+                                <div class="alto_card">
+                                    <img src="assets/img/blog/post_uno.jpg" title="Errores Comunes al crear un sitio Web" class="img-fluid" alt="">
+
+                                    <div class="aparece">
+                                        <ul class="etiquetas_post">
+                                            <li><p>Desarrollo Web</p></li>
+                                            <li><p>UX / UI</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div>
+                                    <h1 class="titulo_post">5 Errores comunes que debes evitar al crear tu sitio web (y cómo solucionarlos)</h1>
+                                    <p class="frase_post">En este post te explico los errores más frecuentes que veo como desarrollador y experto en SEO, y cómo puedes evitarlos.</p>
+                                    <a href="blog/5-errores-comunes-que-etc">
+                                        Ver Más
+                                        <img src="assets/img/ver.png" class="ms-3 negro img-fluid" alt="">
+                                        <img src="assets/img/none.png" class="ms-3 blanco img-fluid" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>        
+    </main>
+
+    <footer>
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            startTypingAnimation();
+        });
+
+        function startTypingAnimation() {
+            var text1 = document.getElementById('text1');
+
+            text1.classList.add('typing');
+        }
+    </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/contenido.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+</body>
+
+</html>
